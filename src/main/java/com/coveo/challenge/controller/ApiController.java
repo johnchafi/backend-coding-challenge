846dd13c60b31af38a218763bbd0ff2d8a9f130e
mcnoreau <marie-christine.noreau.1@ulaval.ca>
2019-04-01 18:23:15 -0400

    chore: refactor + swagger config

---
+++ src/main/java/com/coveo/challenge/controller/ApiController.java
@@ -0,0 +1,20 @@
+package com.coveo.challenge.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@RestController
+public class ApiController {
+
+    @RequestMapping(value = "/suggestions", method = RequestMethod.GET)
+    public ResponseEntity<String> getSuggestions(@RequestParam(value = "q") String query,
+                                                 @RequestParam(value = "longitude", required =  false) Double longitude,
+                                                 @RequestParam(value = "latitude", required = false) Double latitude) {
+        return ResponseEntity.ok("test");
+    }
+}
