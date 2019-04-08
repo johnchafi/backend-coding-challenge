1f44241a9dc2e68a3a16907c02c4a8166be9cc81
Marie-Christine Noreau <Marie-Christine.Noreau@lacapitale.com>
2019-04-08 12:37:28 -0400

    chore: add unit tests

--- src/test/java/com/coveo/challenge/calculator/NameScoreCalculatorTest.java
+++ src/test/java/com/coveo/challenge/calculator/NameScoreCalculatorTest.java
@@ -9,13 +9,32 @@ public class NameScoreCalculatorTest {
     private NameScoreCalculator underTest = new NameScoreCalculator();
 
     @Test
-    public void test() {
-        String query = "Londo";
+    public void whenCalculate_givenSameWord_thenShouldReturnExpected() {
+        String query = "london";
         String name = "London";
         double expected = 1.0;
         double actual = underTest.calculate(query, name);
         assertThat(actual).isEqualTo(expected);
     }
+
+    @Test
+    public void whenCalculate_givenPartialWord_thenShouldReturnExpected() {
+        String query = "mont";
+        String name = "Montréal";
+        double expected = 0.5;
+        double actual = underTest.calculate(query, name);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenCalculate_givenNotSameWord_thenShouldReturnExpected() {
+        String query = "mont";
+        String name = "Quebec";
+        double expected = 0.0;
+        double actual = underTest.calculate(query, name);
+        assertThat(actual).isEqualTo(expected);
+    }
+
 
 }
 
