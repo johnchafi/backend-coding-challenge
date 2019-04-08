6d8cbd61c4bd35a8665161190883674f9eda9e96
Marie-Christine Noreau <Marie-Christine.Noreau@lacapitale.com>
2019-04-08 16:28:15 -0400

    chore: add api doc

--- src/main/java/com/coveo/challenge/controller/SuggestionsController.java
+++ src/main/java/com/coveo/challenge/controller/SuggestionsController.java
@@ -2,7 +2,12 @@ package com.coveo.challenge.controller;
 
 import com.coveo.challenge.model.Suggestions;
 import com.coveo.challenge.service.SuggestionsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.xml.ws.Response;
 
 @RestController
 @Validated
@@ -19,10 +25,18 @@ public class SuggestionsController {
     @Autowired
     private SuggestionsService suggestionsService;
 
+    @ApiOperation(
+            value = "Provides suggestions for large cities"
+    )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "q", required = true, value = "Partial (or complete) search term"),
+            @ApiImplicitParam(name = "latitude", value = "Latitude of caller's location"),
+            @ApiImplicitParam(name = "longitude", value = "Longitude of caller's location")
+    })
     @RequestMapping(value = "/suggestions", method = RequestMethod.GET)
     public Suggestions getSuggestions(@RequestParam(value = "q") String query,
-                                      @RequestParam(value = "latitude", required = false) @Min(-90l) @Max(90l) Double latitude,
-                                      @RequestParam(value = "longitude", required =  false) @Min(-180l) @Max(180l) Double longitude) throws Exception {
+                                                      @RequestParam(value = "latitude", required = false) @Min(-90l) @Max(90l) Double latitude,
+                                                      @RequestParam(value = "longitude", required =  false) @Min(-180l) @Max(180l) Double longitude) throws Exception {
         return suggestionsService.call(query, latitude, longitude);
     }
 }
