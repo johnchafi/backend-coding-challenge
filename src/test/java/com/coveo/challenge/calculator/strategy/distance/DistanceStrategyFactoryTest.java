299a34949e71850d29bbdaae8e03d02b58b85347
Marie-Christine Noreau <Marie-Christine.Noreau@lacapitale.com>
2019-04-08 12:43:28 -0400

    chore: add unit test

---
+++ src/test/java/com/coveo/challenge/calculator/strategy/distance/DistanceStrategyFactoryTest.java
@@ -0,0 +1,23 @@
+package com.coveo.challenge.calculator.strategy.distance;
+
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class DistanceStrategyFactoryTest {
+
+    private DistanceStrategyFactory underTest = new DistanceStrategyFactory();
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGetStrategy_givenInvalidStrategy_thenShouldThrowException() {
+        underTest.getStrategy("invalid");
+    }
+
+    @Test
+    public void whenGetStrategy_givenValidStrategy_thenShouldThrowException() {
+        DistanceStrategy actual = underTest.getStrategy(DistanceStrategyFactory.HAVERSINE);
+        assertThat(actual).isNotNull();
+        assertThat(actual).isInstanceOf(HaversineDistanceStrategy.class);
+    }
+
+}
